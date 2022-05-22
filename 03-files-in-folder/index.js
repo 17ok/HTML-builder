f11693ba41d0ccdfdad864b10d653ad012ca8263
17ok <92027688+17ok@users.noreply.github.com>
2022-05-22 12:01:15 +0300

    feat: tree

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const process = require('process');
+const path = require('path');
+const {readdir, stat} = require('fs');
+
+const folderPath = path.resolve(__dirname, 'secret-folder');
+
+readdir(folderPath, {withFileTypes: true}, (error, fileNames) => {
+    if (error)  throw error;
+    fileNames.forEach( (fileName) => {
+            if (fileName.isFile()) {
+                const filePath = path.resolve(folderPath, fileName.name);
+                const name = path.parse(filePath).name;
+                const ext = path.parse(filePath).ext;
+                stat(filePath, (error, stats) => {
+                    if (error) throw error;
+                    const size = stats.size/1024;
+                    console.log(`${name} - ${ext.slice(1)} - ${size}kb`);
+                })
+            }
+        })
+    }
+    
+)
