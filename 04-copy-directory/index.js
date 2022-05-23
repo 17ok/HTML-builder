a6be1ef4b1a2c994480fa8094f53f8c1ec07adf2
17ok <92027688+17ok@users.noreply.github.com>
2022-05-23 12:17:40 +0300

    feat: four

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const {readdir, mkdir, copyFile, rm} = require('fs/promises');
 
+const folderPath = path.resolve(__dirname, 'files');
+const copyFolderPath = path.resolve(__dirname, 'files-copy');
+
+async function copyFolder(pathOut, pathIn) {
+let fileNames;
+    try {
+    fileNames = await readdir(pathOut, {withFileTypes: true});
+    } catch (error) {
+     throw error;   
+    }
+
+    await rm(pathIn, {recursive: true, force: true});
+    await mkdir(pathIn, {recursive: true});
+
+    fileNames.forEach( (fileName) => {
+        const filePath = path.resolve(pathIn, fileName.name);
+        const copyFilePath = path.resolve(pathOut, fileName.name);
+       // console.log(`${filePath}  -  ${copyFilePath}`);
+       copyFile(copyFilePath, filePath);
+})
+}
+copyFolder(folderPath, copyFolderPath);
