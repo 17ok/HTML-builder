d99c3610eb1173fe2d2558dd324624524a4ed7c7
17ok <92027688+17ok@users.noreply.github.com>
2022-05-23 15:17:49 +0300

    feat: five

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,22 +4,22 @@ const {readdir, mkdir, copyFile, rm} = require('fs/promises');
 const folderPath = path.resolve(__dirname, 'files');
 const copyFolderPath = path.resolve(__dirname, 'files-copy');
 
-async function copyFolder(pathOut, pathIn) {
+async function copyFolder(pathFrom, pathTo) {
 let fileNames;
     try {
-    fileNames = await readdir(pathOut, {withFileTypes: true});
+   fileNames = await readdir(pathFrom, {withFileTypes: true});
     } catch (error) {
      throw error;   
     }
 
-    await rm(pathIn, {recursive: true, force: true});
-    await mkdir(pathIn, {recursive: true});
+    await rm(pathTo, {recursive: true, force: true});
+    await mkdir(pathTo, {recursive: true});
 
     fileNames.forEach( (fileName) => {
-        const filePath = path.resolve(pathIn, fileName.name);
-        const copyFilePath = path.resolve(pathOut, fileName.name);
-       // console.log(`${filePath}  -  ${copyFilePath}`);
-       copyFile(copyFilePath, filePath);
+        const filePath = path.resolve(pathTo, fileName.name);
+        const copyFilePath = path.resolve(pathFrom, fileName.name);
+     // console.log(`${filePath}  -  ${copyFilePath}`);
+      copyFile(copyFilePath, filePath);
 })
 }
 copyFolder(folderPath, copyFolderPath);
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const {readdir} = require('fs/promises');
+const fs = require('fs');
+
+
+const cssCopyTo= path.join(__dirname, 'project-dist', 'bundle.css');
+const cssCopyFrom = path.resolve(__dirname, 'styles');
+
+async function copyStyles(pathTo, pathFrom) {
+    let fileNames;
+    try {
+    fileNames = await readdir(pathFrom, {withFileTypes: true});
+    } catch (error) {
+     throw error;   
+    } 
+    const writeStream = fs.createWriteStream(pathTo, {flags: 'w'});
+    fileNames.forEach( (file) => {
+        const filePath = path.resolve(pathFrom, file.name);
+       
+      if (file.isFile() && path.parse(filePath).ext === '.css') {
+            const readStream = fs.createReadStream(filePath);
+            //readStream.pipe(writeStream, {end: false});
+            readStream.on('data', info => writeStream.write(info + '\n'));
+        }
+    })
+};
+copyStyles(cssCopyTo, cssCopyFrom);
