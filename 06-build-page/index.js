75280e37f40897bded5bcb00275fd043f5ba9388
17ok <92027688+17ok@users.noreply.github.com>
2022-05-23 17:26:04 +0300

    feat: six major part

---
+++ 06-build-page/index.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const {readdir, mkdir, rm, copyFile } = require('fs/promises');
+const fs = require('fs');
+
+
+const folderTo= path.join(__dirname, 'project-dist');
+
+
+async function buildPage(pathTo) {
+  
+     //Создаёт папку project-dist.
+    await rm(pathTo, {recursive: true, force: true});
+    await mkdir(pathTo, {recursive: true});
+
+
+   
+    //Заменяет шаблонные теги в файле template.html
+    //Собирает в единый файл стили из папки styles и помещает их в файл project-dist/style.css.
+    const cssTo = path.join(__dirname, 'project-dist', 'style.css');
+    const cssFrom = path.join(__dirname, 'styles');
+    await copyStyles(cssTo, cssFrom)
+    
+    //Копирует папку assets в project-dist/assets
+    const assetsTo = path.join(__dirname, 'project-dist', 'assets');
+    const assetsFrom = path.join(__dirname, 'assets');
+    try {
+        await copyFolder(assetsTo, assetsFrom);
+    }
+    catch(error){
+        throw error;
+     }
+    
+
+};
+
+//Собирает в единый файл стили из папки styles и помещает их в файл project-dist/style.css.
+async function copyStyles(pathTo, pathFrom) {
+    let fileNames;
+    try {
+    fileNames = await readdir(pathFrom, {withFileTypes: true});
+    } catch (error) {
+     throw error;   
+    } 
+    const writeStream = fs.createWriteStream(pathTo, {flags: 'w'});
+    fileNames.forEach( (file) => {
+        const filePath = path.resolve(pathFrom, file.name);
+       
+      if (file.isFile() && path.parse(filePath).ext === '.css') {
+            const readStream = fs.createReadStream(filePath);
+            //readStream.pipe(writeStream, {end: false});
+            readStream.on('data', info => writeStream.write(info + '\n'));
+        }
+    })
+};
+
+//Копирует папку assets в project-dist/assets
+async function copyFolder(pathTo, pathFrom) {
+        try {
+    const fileNames = await readdir(pathFrom, {withFileTypes: true});
+
+           for (let file of fileNames){
+        if (file.isFile()) {
+            const filePathTo = path.resolve(pathTo, file.name);
+            const filePathFrom = path.resolve(pathFrom, file.name);
+            await copyFile(filePathFrom, filePathTo);
+        } else if (file.isDirectory()){
+            const directoryPathTo = path.resolve(pathTo, file.name);
+            const directoryPathFrom = path.resolve(pathFrom, file.name);
+         await mkdir(directoryPathTo, {recursive: true});
+         await copyFolder(directoryPathTo, directoryPathFrom)
+        }
+    }
+        } catch (error) {
+         throw error;   
+        }
+    
+        //await rm(pathTo, {recursive: true, force: true});
+        await mkdir(pathTo, {recursive: true}); 
+    }
+
+buildPage(folderTo);
